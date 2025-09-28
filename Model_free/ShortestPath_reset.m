c16216f082daf7f3e841e5250cbbcbd294cf689d
Salar Basiri <salarbsr.1996@gmail.com>
2025-09-28 13:40:31 -0500

    Deleted Old Files

--- Model_free/ShortestPath_reset.m
+++
@@ -1,6 +0,0 @@
-function s = ShortestPath_reset(action_count, state_count)
-    s = randi([action_count+1,state_count]);
-    % while s == action_count
-    %     s = randi(action_count);
-    % end
-end
